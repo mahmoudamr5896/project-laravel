4abcabc082c3295bee06cd803617f609c02b1e6d
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2025-05-16 02:28:31 +0300

    up

--- my_app/config/logging.php
+++ my_app/config/logging.php
@@ -83,15 +83,7 @@ return [
         ],
 
         'papertrail' => [
-            'driver' => 'monolog',
-            'level' => env('LOG_LEVEL', 'debug'),
-            'handler' => env('LOG_PAPERTRAIL_HANDLER', SyslogUdpHandler::class),
-            'handler_with' => [
-                'host' => env('PAPERTRAIL_URL'),
-                'port' => env('PAPERTRAIL_PORT'),
-        //   'connectionString' => env('PAPERTRAIL_URL') && env('PAPERTRAIL_PORT') ? 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT') : null,
-            ],
-            'processors' => [PsrLogMessageProcessor::class],
+            
         ],
 
         'stderr' => [
